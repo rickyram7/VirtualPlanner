757e25049f0a2366dc340c1307c6fd8bc36477b4
rickyram7 <ricky.ram7@gmail.com>
2024-02-12 22:02:02 -0600

    Phase 1 changes to the frontend

---
+++ frontend/script.js
@@ -0,0 +1,79 @@
+let nav = 0;
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+
+const calendar = document.getElementById('calendar');
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function load() {
+    const dt = new Date();
+
+    if(nav !== 0) {
+        dt.setMonth(new Date().getMonth() + nav);
+    }
+
+    const day = dt.getDate();
+    const month = dt.getMonth();
+    const year = dt.getFullYear();
+
+
+    const firstDayInMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const dateString = firstDayInMonth.toLocaleDateString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    });
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+    document.getElementById('monthDisplay').innerText = 
+    `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+
+    calendar.innerHTML = '';
+
+    for(let i = 1; i <= paddingDays + daysInMonth; i++) {
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+        const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+        if(i > paddingDays) {
+            daySquare.innerText = i - paddingDays;
+            const eventForDay = events.find(e => e.date === dayString);
+
+            if(i - paddingDays === day && nav === 0) {
+                daySquare.id = 'currentDay';
+            }
+
+            if(eventForDay) {
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText = eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString));
+        } else {
+            daySquare.classList.add('padding');
+        }
+
+        calendar.appendChild(daySquare);
+    }
+}
+
+function initButtons() {
+    document.getElementById('nextButton').addEventListener('click', () => {
+        nav++;
+        document.getElementById('calendar').innerHTML = '';
+        load();
+    });
+    document.getElementById('backButton').addEventListener('click', () => {
+        nav--;
+        document.getElementById('calendar').innerHTML = '';
+        load();
+    });
+}    
+
+initButtons();
+load();    
